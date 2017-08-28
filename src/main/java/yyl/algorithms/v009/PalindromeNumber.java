5292de4fd0980f27b9979e810aba5a118a02eb14
relucent <relucent@163.com>
2017-08-28 11:30:24 +0800

    #

---
+++ src/main/java/yyl/algorithms/v009/PalindromeNumber.java
@@ -0,0 +1,40 @@
+package yyl.algorithms.v009;
+
+/**
+ * Determine whether an integer is a palindrome. Do this without extra space.<br>
+ */
+//判断数字是否是回文数字 
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		int[] samples = { 0, 1, 10, 123321, 1001, 123454321, -1001, 12345, 23456, 123210 };
+		for (int sample : samples) {
+			System.out.println(sample + " isPalindrome = " + isPalindrome(sample));
+		}
+	}
+
+	public static boolean isPalindrome(int x) {
+		if (x < 0) {
+			return false;
+		}
+		long reverse = 0;
+		int y = x;
+		while (y > 0) {
+			reverse = reverse * 10 + (y % 10);
+			y = y / 10;
+		}
+		return reverse == x;
+	}
+
+	public static boolean isPalindrome2(int x) {
+		if (x < 0 || (x != 0 && x % 10 == 0)) {
+			return false;
+		}
+		int y = 0;
+		while (y < x) {
+			y = y * 10 + (x % 10);
+			x = x / 10;
+		}
+		return x == y || x == y / 10;
+	}
+}
