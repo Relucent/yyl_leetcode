9bab7c7168041539a1125befe014c56b812ec749
yyl <relucent@163.com>
2019-12-04 10:59:04 +0800

    #

--- src/main/java/yyl/leetcode/p022/GenerateParentheses.java
+++ src/main/java/yyl/leetcode/p022/GenerateParentheses.java
@@ -4,11 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.x Given n
- * pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * <h3>括号生成</h3> 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。<br>
+ * 例如，给出 n = 3，生成结果为：<br>
  * 
  * <pre>
- * For example, given n = 3, a solution set is: 
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ * </pre>
+ * 
+ * <br>
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.x Given n pairs of parentheses, write a
+ * function to generate all combinations of well-formed parentheses.<br>
+ * For example, given n = 3, a solution set is: <br>
+ * 
+ * <pre>
  * [
  *   "((()))",
  *   "(()())",
@@ -18,37 +32,49 @@ import java.util.List;
  * ]
  * </pre>
  */
-//给定一个非负整数n，生成n对括号的所有合法排列
+// 给定一个非负整数n，生成n对括号的所有合法排列
 public class GenerateParentheses {
 
-	public static void main(String[] args) {
-		Solution solution = new Solution();
-		List<String> result = solution.generateParenthesis(3);
-		System.out.println(result);
-	}
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<String> result = solution.generateParenthesis(3);
+        System.out.println(result);
+    }
 
-	static class Solution {
-		public List<String> generateParenthesis(int n) {
-			List<String> result = new ArrayList<>();
-			generate(new char[n << 1], 0, n, n, result);
-			return result;
-		}
+    static class Solution {
 
-		//定义两个变量left和right分别表示剩余左右括号的个数
-		private void generate(char[] temp, int pos, int leftNum, int rightNum, List<String> result) {
-			if (leftNum == 0 && rightNum == 0) {
-				result.add(new String(temp));
-				return;
-			}
-			if (leftNum > 0) {
-				temp[pos] = '(';
-				generate(temp, pos + 1, leftNum - 1, rightNum, result);
-			}
-			if (rightNum > 0 && rightNum > leftNum) {
-				temp[pos] = ')';
-				generate(temp, pos + 1, leftNum, rightNum - 1, result);
-			}
-		}
-	}
+        /**
+         * 生成所有可能的并且有效的括号组合
+         * @param n 括号对个数
+         * @return 括号组合列表
+         */
+        public List<String> generateParenthesis(int n) {
+            List<String> result = new ArrayList<>();
+            generate(new char[n << 1], 0, n, n, result);
+            return result;
+        }
+
+        /**
+         * @param buffer 字符数组
+         * @param pos 位置
+         * @param leftNum 剩余左括号的个数
+         * @param rightNum 剩余右括号的个数
+         * @param result 结果列表
+         */
+        private void generate(char[] buffer, int pos, int leftNum, int rightNum, List<String> result) {
+            if (leftNum == 0 && rightNum == 0) {
+                result.add(new String(buffer));
+                return;
+            }
+            if (leftNum > 0) {
+                buffer[pos] = '(';
+                generate(buffer, pos + 1, leftNum - 1, rightNum, result);
+            }
+            if (rightNum > 0 && rightNum > leftNum) {
+                buffer[pos] = ')';
+                generate(buffer, pos + 1, leftNum, rightNum - 1, result);
+            }
+        }
+    }
 
 }
