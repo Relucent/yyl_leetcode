a9389ab64161b585d8b795eaa56a25a39bb3c5d6
yyl <relucent@163.com>
2019-12-09 10:21:30 +0800

    +

--- src/main/java/yyl/leetcode/p011/ContainerWithMostWater.java
+++ src/main/java/yyl/leetcode/p011/ContainerWithMostWater.java
@@ -1,66 +1,94 @@
 package yyl.leetcode.p011;
 
 /**
+ * <h3>盛最多水的容器</h3><br>
+ * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。 <br>
+ * 在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。 <br>
+ * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。<br>
+ * 说明：你不能倾斜容器，且 n 的值至少为 2。<br>
+ * 
+ * <pre>
+ * +│-------------------
+ * 8│---■---------■-----
+ * 7│---■~~~~~~~~~■~~~■-
+ * 6│---■-■-------■---■-
+ * 5│---■-■---■---■---■-
+ * 4│---■-■---■-■-■---■-
+ * 3│---■-■---■-■-■-■-■-
+ * 2│---■-■-■-■-■-■-■-■-
+ * 1│-■-■-■-■-■-■-■-■-■-
+ * 0└────────────────────>
+ * -0~1~2~3~4~5~6~7~8~9+
+ * 图中“■”代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（水平面表示“~”）的最大值为 49（7×7）。
+ * </pre>
+ * 
+ * 示例:<br>
+ * 输入: [1,8,6,2,5,4,8,3,7]<br>
+ * 输出: 49<br>
+ * <br>
  * Given n non-negative integers a1, a2, ..., an, where each represents a point at coordinate (i, ai). <br>
  * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). <br>
  * Find two lines, which together with x-axis forms a container, such that the container contains the most water.<br>
  * Note: You may not slant the container and n is at least 2. <br>
  */
-//给定n个非负整数a1,a2,...,an，其中每个代表一个点坐标(i,ai)。 
-//n个垂直线段例如线段的两个端点在(i,ai)和(i,0)。 
-//找到两个线段，与x轴形成一个容器，使其包含最多的水。 
-//备注：你不必倾倒容器，n至少是2
 public class ContainerWithMostWater {
 
-	public static void main(String[] args) {
-		Solution solution = new Solution();
-		int[] height = { 2, 3, 4, 5, 18, 17, 6 };
-		long l = System.currentTimeMillis();
-		for (int i = 0; i < 1001; i++)
-			System.out.println(solution.maxArea(height));
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] height = {2, 3, 4, 5, 18, 17, 6};
+        long l = System.currentTimeMillis();
+        for (int i = 0; i < 1001; i++)
+            System.out.println(solution.maxArea(height));
 
-		System.out.println(System.currentTimeMillis() - l);
-	}
+        System.out.println(System.currentTimeMillis() - l);
+    }
 
-	static class Solution {
-		//1.首先假设我们找到能取最大容积的纵线为 i , j (假定i<j)，那么得到的最大容积 C = min(ai,aj)*(j-i)
-		//2.在 j的右端没有一条线会比它高(因为如果存在 k(j<k&&ak>aj,那么由i,k构成的容器的容积也会大于 C'> C
-		//3.同理，在i的左边也不会有比它高的线
-		//所以我们从两头向中间靠拢，同时更新候选值；在收缩区间的时候优先从 较小的边开始收缩
-		public int maxArea(int[] height) {
-			int left = 0;
-			int right = height.length - 1;
-			int maxArea = 0;
-			while (left < right) {
-				maxArea = Math.max(Math.min(height[left], height[right]) * (right - left), maxArea);
-				if (height[left] < height[right]) {
-					int i = left;
-					while (i < right && height[i] <= height[left]) {
-						i++;
-					}
-					left = i;
-				} else {
-					int i = right;
-					while (i > left && height[i] <= height[right]) {
-						i--;
-					}
-					right = i;
-				}
-			}
-			return maxArea;
-		}
-	}
+    static class Solution {
+        // 双指针法
+        // 1.首先假设我们找到能取最大容积的纵线为 i , j (假定i<j)，那么得到的最大容积 C = min(ai,aj)*(j-i)
+        // 2.在 j的右端没有一条线会比它高(因为如果存在 k(j<k&&ak>aj,那么由i,k构成的容器的容积也会大于 C'> C
+        // 3.同理，在i的左边也不会有比它高的线
+        // 所以我们从两头向中间靠拢，同时更新候选值；在收缩区间的时候优先从 较小的边开始收缩
+        // 时间复杂度 ：O(N2)
+        // 空间复杂度 ：O(1)
+        public int maxArea(int[] height) {
+            int left = 0;
+            int right = height.length - 1;
+            int max = 0;
+            while (left < right) {
+                max = Math.max(Math.min(height[left], height[right]) * (right - left), max);
+                if (height[left] < height[right]) {
+                    int i = left;
+                    while (i < right && height[i] <= height[left]) {
+                        i++;
+                    }
+                    left = i;
+                } else {
+                    int i = right;
+                    while (i > left && height[i] <= height[right]) {
+                        i--;
+                    }
+                    right = i;
+                }
+            }
+            return max;
+        }
+    }
 
-	//暴力方式 , 时间复杂度 O(N^2)
-	static class Solution2 {
-		public int maxArea(int[] height) {
-			int area = 0;
-			for (int i = 0; i < height.length; i++) {
-				for (int j = i + 1; j < height.length; j++) {
-					area = Math.max(area, Math.min(height[i], height[j]) * (j - i));
-				}
-			}
-			return area;
-		}
-	}
+    // 暴力法
+    // 简单地考虑每对可能出现的线段组合并找出这些情况之下的最大面积
+    // 时间复杂度 ：O(N^2)
+    // 空间复杂度 ：O(1)
+    static class Solution2 {
+        public int maxArea(int[] height) {
+            int max = 0;
+            for (int i = 0; i < height.length; i++) {
+                for (int j = i + 1; j < height.length; j++) {
+                    // 一只水桶能装多少水取决于它最短的那块木板，所以高度取 Math.min(a,b)
+                    max = Math.max(Math.min(height[i], height[j]) * (j - i), max);
+                }
+            }
+            return max;
+        }
+    }
 }
--- src/main/java/yyl/leetcode/p016/ThreeSumClosest.java
+++ src/main/java/yyl/leetcode/p016/ThreeSumClosest.java
@@ -3,112 +3,134 @@ package yyl.leetcode.p016;
 import java.util.Arrays;
 
 /**
- * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
- * Return the sum of the three integers. You may assume that each input would have exactly one solution. <br>
+ * <h3>最接近的三数之和</h3><br>
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。<br>
+ * 返回这三个数的和。假定每组输入只存在唯一答案。<br>
+ * 例如，给定数组 nums = [-1，2，1，-4], 和 target = 1.<br>
+ * 与 target 最接近的三个数的和为 2. (-1 + 2 + 1 = 2).<br>
+ * <br>
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three
+ * integers. You may assume that each input would have exactly one solution. <br>
  * For example, given array S = {-1 2 1 -4}, and target = 1. <br>
  * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).<br>
  */
-//给定一个数组的 n个整数，在其中找到3个整数，返回三个整数相加。(要求这三个整数相加与目标最接近)
 public class ThreeSumClosest {
-	public static void main(String[] args) {
-		Solution solution = new Solution();
-		for (TestSample sample : TestSample.values()) {
-			int result = solution.threeSumClosest(sample.nums(), sample.target());
-			System.out.println("answer:" + result + "; expected:" + sample.expected());
-		}
-	}
 
-	static class Solution {
-		public int threeSumClosest(int[] nums, int target) {
-			Arrays.sort(nums);
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.threeSumClosest(new int[] {1, 1, 1}, 0));// 3
+        System.out.println(solution.threeSumClosest(new int[] {-1, 2, 1, -4}, 1));// 2
+        System.out.println(solution.threeSumClosest(new int[] {-55, -24, -18, -11, -7, -3, 4, 5, 6, 9, 11, 23, 33}, 0));// 0
+        System.out.println(solution.threeSumClosest(new int[] {0, 2, 1, -3}, 1));// 0
+    }
 
-			int len = nums.length;
+    // 双指针法的优化
+    // 先让数组有序(对数组排序)，然后每次固定一个元素，再去寻找另外两个元素
+    // 优化部分：
+    // 1.元素重复的问题
+    // 2.超越界限的问题
+    static class Solution {
+        public int threeSumClosest(int[] nums, int target) {
 
-			int min = nums[0] + nums[1] + nums[2];
-			if (min >= target) {
-				return min;
-			}
+            // 利用 Arrays.sort对数组进行排序
+            Arrays.sort(nums);
+            // 初始化一个用于保存结果的值 result = nusm[0] + nums[1] + nums[2]
+            int result = nums[0] + nums[1] + nums[2];
 
-			int max = nums[len - 3] + nums[len - 2] + nums[len - 1];
-			if (max <= target) {
-				return max;
-			}
+            // 利用下标 i 对数组进行遍历，此时就是在固定第一个元素，注意，下标 i 的边界为 i < nums.length-2，否则设置指针的时候会出现数组越界
+            for (int i = 0; i < nums.length - 2; i++) {
 
-			for (int i = 0; i < len - 2; i++) {
-				int minT = nums[i] + nums[i + 1] + nums[i + 2];
-				if (minT > target) {
-					if (minT < max) {
-						max = minT;
-					}
-					break;
-				}
+                // 每次遍历的过程中设置两个指针，分别是 left = i + 1、right = nums.length - 1
+                int left = i + 1;
+                int right = nums.length - 1;
 
-				int maxT = nums[i] + nums[len - 2] + nums[len - 1];
-				if (maxT < target) {
-					if (maxT > min) {
-						min = maxT;
-					}
-					continue;
-				}
+                // 双指针遍历 (如果 left==right，说明我们已经将所有的元素都遍历过一遍了)
+                while (left < right) {
+                    // 判断最小值(优化点)
+                    int min = nums[i] + nums[left] + nums[left + 1];
+                    if (target < min) {
+                        if (Math.abs(result - target) > Math.abs(min - target)) {
+                            result = min;
+                        }
+                        break;
+                    }
 
-				int left = i + 1;
-				int right = len - 1;
-				while (left < right) {
-					int sum = nums[left] + nums[right] + nums[i];
-					if (sum == target) {
-						return target;
-					} else if (sum < target) {
-						if (min < sum) {
-							min = sum;
-						}
-						left++;
-					} else {
-						if (max > sum) {
-							max = sum;
-						}
-						right--;
-					}
-				}
-			}
+                    // 判断最大值(优化点)
+                    int max = nums[i] + nums[right] + nums[right - 1];
+                    if (target > max) {
+                        if (Math.abs(result - target) > Math.abs(max - target)) {
+                            result = max;
+                        }
+                        break;
+                    }
 
-			if (max == target || min == target) {
-				return target;
-			}
-			return (max - target) > (target - min) ? min : max;
-		}
-	}
+                    // 获得三个数之和
+                    int sum = nums[i] + nums[left] + nums[right];
+                    // 如果 sum 之前保存的 result与 target 更接近就更新 result
+                    if (Math.abs(sum - target) < Math.abs(result - target)) {
+                        result = sum;
+                    }
+                    // 移动双指针
+                    // 如果 sum 的值比 target 大，那么 right--
+                    if (sum > target) {
+                        right--;
+                        // 元素重复，继续移动指针(优化点)
+                        while (left != right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    }
+                    // 如果 sum 的值 target 小，那么 left++
+                    else {
+                        left++;
+                        // 元素重复，继续移动指针(优化点)
+                        while (left != right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
 
-	static class Solution2 {
-		public int threeSumClosest(int[] nums, int target) {
-			Arrays.sort(nums);
-			int closest = 0;
-			int minDifference = Integer.MAX_VALUE;
-			for (int i = 0, I = nums.length - 2, R = nums.length - 1; i < I; i++) {
-				int left = i + 1;
-				int right = R;
-				while (left < right) {
-					int sum = nums[i] + nums[left] + nums[right];
-					if (sum == target) {
-						return sum;
-					}
-					if (sum > target) {
-						int difference = sum - target;
-						if (difference < minDifference) {
-							minDifference = difference;
-							closest = sum;
-						}
-						right--;
-					} else {
-						int difference = target - sum;
-						if (difference < minDifference) {
-							minDifference = difference;
-							closest = sum;
-						}
-						left++;
-					}
-				}
-			}
-			return closest;
-		}
-	}
+    // 双指针法
+    // 先让数组有序(对数组排序)，然后每次固定一个元素，再去寻找另外两个元素
+    static class Solution2 {
+        public int threeSumClosest(int[] nums, int target) {
+            // 利用 Arrays.sort对数组进行排序
+            Arrays.sort(nums);
+            // 初始化一个用于保存结果的值 result = nusm[0] + nums[1] + nums[2]
+            int result = nums[0] + nums[1] + nums[2];
+            // 利用下标 i 对数组进行遍历，此时就是在固定第一个元素，注意，下标 i 的边界为 i < nums.length-2，否则设置指针的时候会出现数组越界
+            for (int i = 0; i < nums.length; i++) {
+                // 每次遍历的过程中设置两个指针，分别是 left = i + 1、right = nums.length - 1
+                int left = i + 1;
+                int right = nums.length - 1;
+                // 双指针遍历 (如果 left==right，说明我们已经将所有的元素都遍历过一遍了)
+                while (left < right) {
+                    // 获得三个数之和 sum = nums[i] + nums[left] + nums[right]
+                    int sum = nums[i] + nums[left] + nums[right];
+
+                    // 如果三个数之和与目标相等那么直接返回(相等必然最接近)
+                    if (sum == target) {
+                        return sum;
+                    }
+                    // 如果 sum 之前保存的 result与 target 更接近就更新 result
+                    if (Math.abs(sum - target) < Math.abs(result - target)) {
+                        result = sum;
+                    }
+                    // 移动双指针
+                    // 如果 sum 的值比 target 大，那么 right--，因为数组是有序的，right --会使得下一次的 sum 更小，也就更接近 target 的值
+                    if (sum > target) {
+                        right--;
+                    }
+                    // 否则，如果 sum 的值 target 小，那么 left++
+                    else {
+                        left++;
+                    }
+                }
+            }
+            return result;
+        }
+    }
 }
