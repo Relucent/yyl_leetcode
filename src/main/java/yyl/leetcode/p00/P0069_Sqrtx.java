e795bd01f51304e7fc3cc595ab3b401e4d9e4c02
yyl <relucent@163.com>
2020-01-01 17:33:37 +0800

    +

--- src/main/java/yyl/leetcode/p00/P0069_Sqrtx.java
+++ src/main/java/yyl/leetcode/p00/P0069_Sqrtx.java
@@ -18,12 +18,63 @@ package yyl.leetcode.p00;
  * </pre>
  */
 public class P0069_Sqrtx {
+
     public static void main(String[] args) {
-        Solution3 solution = new Solution3();
-        System.out.println(solution.mySqrt(1));
-        System.out.println(solution.mySqrt(4));
-        System.out.println(solution.mySqrt(10));
-        System.out.println(solution.mySqrt(2147395599));
+        Solution1 solution = new Solution1();
+        System.out.println(solution.mySqrt(0));// 0
+        System.out.println(solution.mySqrt(1));// 1
+        System.out.println(solution.mySqrt(4));// 2
+        System.out.println(solution.mySqrt(10));// 3
+        System.out.println(solution.mySqrt(2147395599));// 46339
+    }
+
+
+    // 牛顿法求解平方根
+    // 牛顿迭代法（Newton's method）又称为牛顿-拉夫逊（拉弗森）方法（Newton-Raphson method），它是牛顿在17世纪提出的一种在实数域和复数域上近似求解方程的方法。
+    // 求 x 的平方根，首先猜测一个值z=x/2，然后根据迭代公式 z{n+1} = (z{n} + x/z{n})/2，算出z2，再将z2代公式的右边算出z3
+    // 通过反复迭代，z 趋近于x的平方根 ，当 z^2与x 的差的绝对值小于某个值，即认为找到了精确的平方根。
+    // 牛顿迭代法一般不讨论其时间复杂度，迭代法的总的计算量由迭代次数决定，而迭代次数与阈值、初始值、收敛后的值有关。
+    // 开平方所需的迭代次数随被开方数的大小变化，当被开方数范围在10-1000之间时，迭代次数仅为6-9次。
+    // 时间复杂度：O(1)，时间复杂度与精度有关，一般情况可以认为是常数级别
+    // 空间复杂度是：O(1)
+    static class Solution {
+        public int mySqrt(int x) {
+            if (x == 0 || x == 1) {
+                return x;
+            }
+            double z = x;
+            while (Math.abs(z * z - x) >= 0.05) {
+                z = (z + x / z) / 2;
+            }
+            return (int) z;
+        }
+    }
+
+    // 牛顿法求解平方根
+    // 牛顿迭代法（Newton's method）又称为牛顿-拉夫逊（拉弗森）方法（Newton-Raphson method），它是牛顿在17世纪提出的一种在实数域和复数域上近似求解方程的方法。
+    // 假设欲求x的平方根，首先猜测一个值z=x/2，然后根据迭代公式 z{n+1} = (z{n} + x/z{n})/2，算出z2，再将z2代公式的右边算出z3，然后继续迭代计算，直到 z{n}和z{n+1}的差的绝对值小于某个值，即认为找到了精确的平方根。
+    // 这个算法比上一个算法少了一个平方运算，并且更为求的解更加精确
+    // 牛顿迭代法一般不讨论其时间复杂度，迭代法的总的计算量由迭代次数决定，而迭代次数与阈值、初始值、收敛后的值有关。
+    // 根据牛顿法的原理可知，迭代的次数越多，近似值越逼近真实值，当然我们会通过设置精度来限制它的迭代次数。
+    // 时间复杂度：O(1) ~ O(logN)，牛顿法每次迭代的误差至少会减少一半，一般情况时间复杂度为常数级别，最差情况复杂度也会小于O(logN)。
+    // 空间复杂度是：O(1)
+    static class Solution1 {
+        public int mySqrt(int x) {
+            if (x < 0) {
+                return 0;
+            }
+            if (x == 0 || x == 1) {
+                return x;
+            }
+            double e = 1e-15;
+            double z = x;
+            double y = (z + x / z) / 2;
+            while (Math.abs(z - y) > e) {
+                z = y;
+                y = (z + x / z) / 2;
+            }
+            return (int) z;
+        }
     }
 
     // 穷举法(时间复杂度很高，不推荐使用)
@@ -48,7 +99,7 @@ public class P0069_Sqrtx {
     // 二分法：穷举法的优化，使用中间数作为(例如x/2)作为猜测值来运算
     // 如果结果大了，说明解在区间(0，x/2)之间，如果结果小了，说明解在区间(x/2，x)，以此类推
     // 运算过程中涉及到乘法运算时同样需要强转为long类型防止越界
-    // 时间复杂度是：O(log(x/2))
+    // 时间复杂度是：O(log(x))
     // 空间复杂度是：O(1)。
     static class Solution3 {
         public int mySqrt(int x) {
