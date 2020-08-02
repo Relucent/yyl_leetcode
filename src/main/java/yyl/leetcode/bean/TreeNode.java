4c0e6fb77cf934c3998dcf03efdddf890e59f63d
yyl <relucent@163.com>
2020-08-02 12:46:32 +0800

    +

--- src/main/java/yyl/leetcode/bean/TreeNode.java
+++ src/main/java/yyl/leetcode/bean/TreeNode.java
@@ -19,6 +19,45 @@ public class TreeNode {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((left == null) ? 0 : left.hashCode());
+		result = prime * result + ((right == null) ? 0 : right.hashCode());
+		result = prime * result + val;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		TreeNode other = (TreeNode) obj;
+		if (left == null) {
+			if (other.left != null) {
+				return false;
+			}
+		} else if (!left.equals(other.left)) {
+			return false;
+		}
+		if (right == null) {
+			if (other.right != null) {
+				return false;
+			}
+		} else if (!right.equals(other.right)) {
+			return false;
+		}
+		return val == other.val;
+	}
+
+	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		Deque<TreeNode> queue = new ArrayDeque<>();
